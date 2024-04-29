0b9d44557d78812d9c5d9ac520246acf29e25429
Lakshmi Sarvani Nimmagadda <2200030731@kluniversity.in>
2024-04-29 19:34:17 +0530

    changed .env file

--- Server.js
+++ Server.js
@@ -2,19 +2,29 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")//allows one app with another app to resource sharing
 
-const dburl = "mongodb://localhost:27017/studentcourseregistration" // if data base is not there it we create new one
-mongoose.connect(dburl).then(() => {
-    console.log("Connected to db successfully")
-}).catch((err) => {
-    console.log(err.message)
-});
+// const dburl = "mongodb://localhost:27017/studentcourseregistration" // if data base is not there it we create new one
+// mongoose.connect(dburl).then(() => {
+//     console.log("Connected to db successfully")
+// }).catch((err) => {
+//     console.log(err.message)
+// });
+
+/*Atlas COnnection */
+
+
+ const dburl = "mongodb+srv://admin:admin@cluster0.vuqxjga.mongodb.net/studentcourseregistration?retryWrites=true&w=majority&appName=Cluster0"
+ mongoose.connect(dburl).then(() => {
+     console.log("Connected to MongoDB Atlas successfully")
+ }).catch((err) => {
+     console.log(err.message)
+ });
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
 const studentrouter = require("./routes/studentroutes");
- const facultyrouter = require("./routes/facultyroutes");
+ const facultyrouter = require("./routes/Facultyroutes");
 //const courserouter = require("./routes/courseroute");
 const adminrouter = require("./routes/adminroutes");
 
